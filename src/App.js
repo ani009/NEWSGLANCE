aae1149ded453c806cd38d65592d0bc36d8e4863
ANIKET PAL <92258633+ani009@users.noreply.github.com>
2024-02-12 21:31:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import Navbar from './components/Navbar';
 import Newscontent from './components/Newscontent';
 import { useState } from 'react';
 import Footer from './components/Footer';
+import {Routes,Route} from 'react-router-dom';
 import './App.css'
 const App=()=>{
   const [category,setcategory]=useState([]);
   return (
     <>
-    <Navbar setcategory={setcategory}/>
-    <Newscontent category={category}/>
-    <Footer/>
+    <Routes>
+      <Route path='/NEWSGLANCE/' element={
+        <>
+        <Navbar setcategory={setcategory}/>
+        <Newscontent category={category}/>
+        <Footer/>
+        </>
+      } />
+    </Routes>
     </>
   );
 }
